5db64cf49a298e822c55044107522437e8ddbd44
Vlad48-star <85586492+Vlad48-star@users.noreply.github.com>
2022-06-05 15:30:38 +0300

    feat: add read, transform, write streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,19 @@
+import * as path from "path";
+import * as fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const container = path.join(__dirname, "files");
+const fileToReadPath = path.join(container, "fileToRead.txt");
+
 export const read = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(fileToReadPath, "utf8");
+  readStream.on("data", (data) => {
+    process.stdout.write(data);
+  });
 };
+
+read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,17 @@
-export const transform = async () => {
-    // Write your code here 
+import { Transform } from "stream";
+
+const reverseTransform = () => {
+  return new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(chunk.toString().split("").reverse().join(""));
+      callback();
+      process.exit();
+    },
+  });
 };
+
+export const transform = async () => {
+  process.stdin.pipe(reverseTransform()).pipe(process.stdout);
+};
+
+transform();
