45e562e9280645ab01b7179165390684f83135c8
Vlad48-star <85586492+Vlad48-star@users.noreply.github.com>
2022-06-05 11:43:35 +0300

    feat: add copy and create fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,49 @@
-export const copy = async () => {
-    // Write your code here 
+import * as path from "path";
+import * as fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const container = path.join(__dirname, "files");
+const copyFileContainer = path.join(__dirname, "files_copy");
+
+const deepCopyDir = async (itemPath, copyItemPath) => {
+  await fs.promises.mkdir(
+    copyItemPath,
+    {
+      recursive: true,
+    },
+    (err) => {
+      if (err) throw err;
+    }
+  );
+
+  const items = await fs.promises.readdir(itemPath, {
+    withFileTypes: true,
+  });
+
+  for (let item of items) {
+    const srcPath = path.join(itemPath, item.name);
+    const destPath = path.join(copyItemPath, item.name);
+
+    if (item.isDirectory()) {
+      await deepCopyDir(srcPath, destPath);
+    } else {
+      await fs.promises.copyFile(srcPath, destPath);
+    }
+  }
 };
+
+const copy = async (filePath, copyFilePath) => {
+  fs.access(copyFilePath, function (error) {
+    if (error) {
+      deepCopyDir(filePath, copyFilePath);
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
+};
+
+copy(container, copyFileContainer);
